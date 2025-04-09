fbd734c73292bb61e4bca98c67055f8bf9de5d31
khaledssbd <khaledssbd@gmail.com>
2025-04-09 14:33:43 +0600

    pagination done

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,14 +1,32 @@
 import { Request, Response } from 'express';
 import { adminService } from './admin.service';
+import { pick } from '../../../shared/pick';
+import { adminFilterableFields } from './admin.constant';
 
 const getAllAdmin = async (req: Request, res: Response) => {
-  const result = adminService.getAllAdmin();
+  try {
+    const filters = pick(req.query, adminFilterableFields);
+    const paginationOptions = pick(req.query, [
+      'limit',
+      'page',
+      'sortBy',
+      'sortOrder',
+    ]);
+    const result = adminService.getAllAdminFromDB(filters, paginationOptions);
 
-  res.status(200).json({
-    success: true,
-    message: 'Admins fetched successfully!',
-    data: result,
-  });
+    res.status(200).json({
+      success: true,
+      message: 'Admins fetched successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || 'Something went wrong!',
+      error: err,
+    });
+  }
 };
 
 export const adminController = {
---
+++ src/helpers/paginationHelper.ts
@@ -0,0 +1,24 @@
+type TOptions = {
+  page?: string;
+  limit?: string;
+  sortBy?: string;
+  sortOrder?: 'asc' | 'desc';
+};
+
+type TPagiReturn = {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: 'asc' | 'desc';
+};
+
+export const calculatePagination = (options: TOptions): TPagiReturn => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 10;
+  const skip: number = (page - 1) * limit;
+
+  const sortBy: string = (options.sortBy as string) || 'createdAt';
+  const sortOrder: 'asc' | 'desc' = options.sortOrder || 'desc';
+  return { page, limit, skip, sortBy, sortOrder };
+};
