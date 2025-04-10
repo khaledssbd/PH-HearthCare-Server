a629860d528c41b3b12e74d77a9828463a0fb4e5
khaledssbd <khaledssbd@gmail.com>
2025-04-11 00:48:32 +0600

    final

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from 'express';
 import { adminService } from './admin.service';
 import { pick } from '../../../shared/pick';
-import { adminFilterableFields, paginationFields } from './admin.constant';
+import { adminFilterableFields } from './admin.constant';
+import { paginationFields } from '../../../helpers/paginationHelper';
 
 const getAllAdmin = async (req: Request, res: Response) => {
   try {
     const filters = pick(req.query, adminFilterableFields);
     const paginationOptions = pick(req.query, paginationFields);
-    const result = adminService.getAllAdminFromDB(filters, paginationOptions);
+    const result = await adminService.getAllAdminFromDB(
+      filters,
+      paginationOptions
+    );
 
     res.status(200).json({
       success: true,
       message: 'Admins fetched successfully!',
-      data: result,
+      data: result.data,
+      meta: result.meta,
     });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
